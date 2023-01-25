0d8a8fb586d85669c31ab331f3ee8bd0665f37aa
DevBack <heltonlima.ti@live.com>
2023-01-25 16:36:19 -0300

    Create PersonRespository

---
+++ src/main/java/com/devback/persons/repositories/PersonRepository.java
@@ -0,0 +1,11 @@
+package com.devback.persons.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.devback.persons.entities.Person;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
+}
